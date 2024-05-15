ab535b82dfbbe700245fd71b6e7babc69a22b08c
Riku Usui <rick.2000u@gmail.com>
2024-05-15 14:14:26 +0900

    :add: Bootstrapファイルの追加 :add: composer.jsonファイルの追加 :add: twigフォルダの追加 :update: nginx, phpのDockerfileの修正(Twigの追加等)

---
+++ src/Bootstrap.class.php
@@ -0,0 +1,29 @@
+<?php
+namespace Src;
+
+class Bootstrap
+{
+  const DB_HOST = 'localhost';
+  const DB_NAME = 'photostudio_db';
+  const DB_USER = 'photostudio_user';
+  const DB_PASS = 'photostudio_pass';
+  const DB_TYPE = 'mysql';
+  const APP_DIR =  __DIR__;
+  const TEMPLATE_DIR = __DIR__ . '/../template/';
+  const CACHE_DIR = false;
+  const APP_URL = 'http://localhost:8080/';
+
+  public static function loadClass($class)
+  {
+
+    // クラスファイルのパスを生成
+    $path = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+
+    // クラスファイルが存在する場合、読み込む
+    if (file_exists($path)) {
+      require_once $path;
+    }
+  }
+}
+
+spl_autoload_register(['Src\Bootstrap', 'loadClass']);
--- src/index.php
+++ src/index.php
@@ -1,2 +1,8 @@
 <?php
-phpinfo();
+namespace Src;
+
+require_once __DIR__ ."/Bootstrap.class.php";
+
+use Src\Bootstrap;
+
+var_dump(Bootstrap::DB_HOST);
